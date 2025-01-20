2d7114745a17a5a8be045645b52ca200b36e1e45
PrudhviKleza <prudhvi.macherla@kleza.io>
2025-01-21 04:02:33 +0530

    implemented feature2

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import { useState } from 'react'
 function App() {
   const [count, setCount] = useState(0)
 
+  const showAlert = () => {
+    setCount((count) => count + 1)
+    alert('Counter value ${count}');
+  };
   return (
     <>
      
       <div className="card">
         <h1>count is {count}</h1>
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={showAlert }>
           Increment Button
         </button>
         
